5ffd6eb8435bdb0d1a94430f109d9521a56e08d3
jsh424 <jsh911424@catholic.ac.kr>
2026-06-30 22:51:38 +0900

    setting
    setting 1

---
+++ SeniorON/src/test/java/com/example/senioron/SeniorOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senioron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
