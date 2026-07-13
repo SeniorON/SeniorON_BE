1635f679ad03df0f12a84e3a42a02b801768f4db
Gibeom <kiuten12345@naver.com>
2026-07-13 15:08:19 +0900

    fix : 도로명 주소 레코드 추가

--- SeniorON/src/main/java/com/example/senioron/domain/event/dto/response/KakaoAddressResponse.java
+++ SeniorON/src/main/java/com/example/senioron/domain/event/dto/response/KakaoAddressResponse.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record KakaoAddressResponse(List<Document> documents){
-    public record Document(Address address){}
+    public record Document(Address address, RoadAddress roadAddress){}
     public record Address(
             @JsonProperty("address_name") String addressName
+    ){}
+    public record RoadAddress(
+            @JsonProperty("address_name") String roadAddress
     ){}
 }
