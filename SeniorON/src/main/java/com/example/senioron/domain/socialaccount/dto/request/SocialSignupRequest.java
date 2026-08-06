56c4333320a04ec623b0c5d915b6abe123e88c7e
lilyyang0077 <lilyyang0077@test.com>
2026-08-06 18:54:20 +0900

    fix: User 부분 validation message 추가

--- SeniorON/src/main/java/com/example/senioron/domain/socialaccount/dto/request/SocialSignupRequest.java
+++ SeniorON/src/main/java/com/example/senioron/domain/socialaccount/dto/request/SocialSignupRequest.java
@@ -12,30 +12,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SocialSignupRequest {
 
-    @NotNull
+    @NotNull(message = "로그인 제공자를 선택해 주세요.")
     private LoginProvider provider;
 
     private String socialToken;
 
-    @NotBlank
+    @NotBlank(message = "이름을 입력해 주세요.")
     private String name;
 
-    @NotNull
+    @NotNull(message = "생년월일을 입력해 주세요.")
     private LocalDate birth;
 
-    @NotNull
+    @NotNull(message = "회원 역할을 선택해 주세요.")
     private Role role;
 
-    @NotNull
+    @NotNull(message = "서비스 이용약관 동의 여부를 입력해 주세요.")
     private Boolean serviceTermsAgreed;
 
-    @NotNull
+    @NotNull(message = "개인정보 처리방침 동의 여부를 입력해 주세요.")
     private Boolean privacyPolicyAgreed;
 
-    @NotNull
+    @NotNull(message = "만 14세 이상 동의 여부를 입력해 주세요.")
     private Boolean ageOver14Agreed;
 
-    @NotNull
+    @NotNull(message = "마케팅 정보 수신 동의 여부를 입력해 주세요.")
     private Boolean marketingAgreed;
 
     private String fcmToken;
