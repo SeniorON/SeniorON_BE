9ee341b0e22e20d62c15164dd179c66d90d8b179
seunghyun <jsh911424@gmail.com>
2026-09-11 22:58:31 +0900

    Setting: 애플리케이션 보안 점검 및 강화

--- SeniorON/src/main/java/com/example/senioron/global/config/SecurityConfig.java
+++ SeniorON/src/main/java/com/example/senioron/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.senioron.global.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,6 +43,7 @@ public class SecurityConfig {
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
